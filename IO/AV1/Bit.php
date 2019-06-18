0557a7510e4bc51dbb3a63efed76cd2c15d2af6c
Yoshihiro Yamazaki <yoya@awm.jp>
2019-06-18 12:41:06 +0900

    get_offset, set_offset, need bitOffset == 0, maybe.

--- IO/AV1/Bit.php
+++ IO/AV1/Bit.php
@@ -14,9 +14,17 @@ if (is_readable('vendor/autoload.php')) {
 
 class IO_AV1_Bit extends IO_Bit {
     function get_position() {
-        return $this->getOffset()[0];
+        list($byteOffset, $bitOffset) = $this->getOffset();
+        if ($bitOffset != 0) {
+            throw new Exception("get_position: curr bitOffset:$bitOffset != 0");
+        }
+        return $byteOffset;
     }
     function set_position($posi) {
+        list($byteOffset, $bitOffset) = $this->getOffset();
+        if ($bitOffset != 0) {
+            throw new Exception("set_position: curr bitOffset:$bitOffset != 0");
+        }
         return $this->setOffset($posi, 0);
     }
     function get_f($n) {
